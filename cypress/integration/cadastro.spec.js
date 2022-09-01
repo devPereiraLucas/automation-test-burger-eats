c3c19e3ed36d911279500b77714119adda3f102a
Lucas <lucaspereira.desenvolvedor@gmail.com>
2022-08-31 23:35:27 -0300

    sending registration and validating confirmation popup

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -40,6 +40,11 @@ describe('Cadastro', ()=> {
         cy.contains('.delivery-method li', entregador.metodo_entrega).click()
 
         cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh)
+
+        cy.get('form button[type="submit"]').click()
+
+        const expected_message = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        cy.get('.swal2-container .swal2-html-container').should('have.text', expected_message)
         
     })
 })
