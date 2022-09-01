59b892202537d97657fedeb864cfc7188128d456
Lucas <lucaspereira.desenvolvedor@gmail.com>
2022-09-01 16:44:53 -0300

    PO strategy implemetation for registration page

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,10 +1,7 @@
+import SignupPage from '../pages/SignupPage'
+
 describe('Cadastro', ()=> {
     it('Usuario deve se tornar um entregador', ()=> {
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-        
         var deliver = {
             name: 'Lucas Pereira',
             cpf: '10903587589',
@@ -22,29 +19,14 @@ describe('Cadastro', ()=> {
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name)
-        cy.get('input[name="cpf"]').type(deliver.cpf)
-        cy.get('input[name="email"]').type(deliver.email)
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
-        cy.get('input[type="button"][value="Buscar CEP"]').click()
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
 
-        cy.get('input[name="address-number"]').type(deliver.address.number)
-        cy.get('input[name="address-details"]').type(deliver.address.details)
-
-        cy.get('input[name="address"]').should('have.value', deliver.address.street)
-        cy.get('input[name="district"]').should('have.value', deliver.address.district)
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
-
-        cy.get('form button[type="submit"]').click()
-
-        const expected_message = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-        cy.get('.swal2-container .swal2-html-container').should('have.text', expected_message)
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
         
     })
 
@@ -71,29 +53,13 @@ describe('Cadastro', ()=> {
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name)
-        cy.get('input[name="cpf"]').type(deliver.cpf)
-        cy.get('input[name="email"]').type(deliver.email)
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
-        cy.get('input[type="button"][value="Buscar CEP"]').click()
-
-        cy.get('input[name="address-number"]').type(deliver.address.number)
-        cy.get('input[name="address-details"]').type(deliver.address.details)
-
-        cy.get('input[name="address"]').should('have.value', deliver.address.street)
-        cy.get('input[name="district"]').should('have.value', deliver.address.district)
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
-
-        cy.get('form button[type="submit"]').click()
-
-        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
-        
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
+        signup.alertMessageShouldBe('Oops! CPF inválido')
+       
     })
 
 })
