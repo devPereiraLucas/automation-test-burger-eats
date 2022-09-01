442e2f4ed4b308622878510e5d4cc7570f8e4c5b
Lucas <lucaspereira.desenvolvedor@gmail.com>
2022-09-01 15:46:09 -0300

    creating incorrect cpf case test

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -48,4 +48,52 @@ describe('Cadastro', ()=> {
         
     })
 
+    it('CPF Incorreto', ()=> {
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+        
+        var deliver = {
+            name: 'Lucas Pereira',
+            cpf: '10903587AAD',
+            email: 'lucas@gmail.com',
+            whatsapp: '11999999999',
+            address: {
+                postalcode: '88350610',
+                street: 'Rua Frederico Petrusky',
+                number: '1347',
+                details: 'Casa',
+                district: 'Guarani',
+                city_state: 'Brusque/SC', 
+            },
+            delivery_method: 'Moto',
+            cnh: 'cnh-digital.jpg'
+        }
+
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
+        cy.get('input[type="button"][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
+
+        cy.get('input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
+
+        cy.contains('.delivery-method li', deliver.delivery_method).click()
+
+        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
+
+        cy.get('form button[type="submit"]').click()
+
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+        
+    })
+
 })
