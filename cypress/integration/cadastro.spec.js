221333eede8dbfe62275aafd04dd1de8cd2664d1
Lucas <lucaspereira.desenvolvedor@gmail.com>
2022-09-02 11:04:39 -0300

    implementing external test data

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,61 +1,27 @@
 import signup from '../pages/SignupPage'
 
-describe('Cadastro', ()=> {
-    it('Usuario deve se tornar um entregador', ()=> {
-        var deliver = {
-            name: 'Lucas Pereira',
-            cpf: '10903587589',
-            email: 'lucas@gmail.com',
-            whatsapp: '11999999999',
-            address: {
-                postalcode: '88350610',
-                street: 'Rua Frederico Petrusky',
-                number: '1347',
-                details: 'Casa',
-                district: 'Guarani',
-                city_state: 'Brusque/SC', 
-            },
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
+describe('Cadastro', () => {
 
+    beforeEach(function() {
+        cy.fixture('deliver').then((d) => {
+            this.deliver = d
+        })
+    })
+
+    it('Usuario deve se tornar um entregador', function() {
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.signup)
         signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
         signup.modalContentShouldBe(expectedMessage)
-        
     })
 
-    it('CPF Incorreto', ()=> {
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-        
-        var deliver = {
-            name: 'Lucas Pereira',
-            cpf: '10903587AAD',
-            email: 'lucas@gmail.com',
-            whatsapp: '11999999999',
-            address: {
-                postalcode: '88350610',
-                street: 'Rua Frederico Petrusky',
-                number: '1347',
-                details: 'Casa',
-                district: 'Guarani',
-                city_state: 'Brusque/SC', 
-            },
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
-
+    it('CPF Incorreto', function() {
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.cpf_inv)
         signup.submit()
-        signup.alertMessageShouldBe('Oops! CPF inválido')
-       
+        signup.alertMessageShouldBe('Oops! CPF inválido') 
     })
 
 })
