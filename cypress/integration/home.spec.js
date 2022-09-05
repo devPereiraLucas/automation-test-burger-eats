85c9c016c76346e5fb234f699a471ad4810f1a18
Lucas <lucaspereira.desenvolvedor@gmail.com>
2022-09-05 10:23:53 -0300

    changing name patronization of test case

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,5 @@
 describe('home page', ()=> {
-    it('app deve estar online', ()=> {
+    it('app must be online', ()=> {
         cy.viewport(1440, 900)
         cy.visit('https://buger-eats.vercel.app')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
